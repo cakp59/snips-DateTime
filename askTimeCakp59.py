034ba611731bca20cc9b0c75f904f531a50c1f6a
cakp59 <50278507+cakp59@users.noreply.github.com>
2019-05-26 20:59:42 +0200

    Update askTimeCakp59.py

--- askTimeCakp59.py
+++ askTimeCakp59.py
@@ -5,7 +5,6 @@ import configparser
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
-import io
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
